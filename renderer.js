927e4cfb75ae55f32ee16312dcf32c909ecb5794
Mario <contacto@mariodiezgarcia.com>
2019-02-05 23:37:52 +0100

    first request done

---
+++ renderer.js
@@ -0,0 +1,18 @@
+const config = {
+    'appId': 'appId',
+    'apiKey': 'apiKey'
+}
+
+fetch(`https://api.tmb.cat/v1/ibus/lines/6/stops/3256?app_id=${config.appId}&app_key=${config.apiKey}`)
+    .then(data => data.json())
+    .then((bus) => {
+        const arrayLength = bus.data.ibus.length;
+        console.log(bus)
+        setTimeout(() => {
+            if (arrayLength > 0) {
+                document.querySelector('.time').innerHTML = bus.data.ibus[0]["text-ca"]
+            } else {
+                document.querySelector('.time').innerHTML = "No information"
+            }
+        }, 3000);
+    })
