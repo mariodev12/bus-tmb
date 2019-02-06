1eb89ad052e4dc5c22301124069f0bf7bfc32d74
Mario <contacto@mariodiezgarcia.com>
2019-02-06 22:11:27 +0100

    added icon and interval 30s for every request

--- renderer.js
+++ renderer.js
@@ -3,16 +3,26 @@ const config = {
     'apiKey': 'apiKey'
 }
 
-fetch(`https://api.tmb.cat/v1/ibus/lines/6/stops/3256?app_id=${config.appId}&app_key=${config.apiKey}`)
-    .then(data => data.json())
-    .then((bus) => {
-        const arrayLength = bus.data.ibus.length;
-        console.log(bus)
-        setTimeout(() => {
-            if (arrayLength > 0) {
-                document.querySelector('.time').innerHTML = bus.data.ibus[0]["text-ca"]
-            } else {
-                document.querySelector('.time').innerHTML = "No information"
-            }
-        }, 3000);
-    })
+window.onload = () => {
+    get();
+}
+
+const get = () => {
+    fetch(`https://api.tmb.cat/v1/ibus/lines/6/stops/3256?app_id=${config.appId}&app_key=${config.apiKey}`)
+        .then(data => data.json())
+        .then((bus) => {
+            const arrayLength = bus.data.ibus.length;
+            console.log(bus)
+            setTimeout(() => {
+                if (arrayLength > 0) {
+                    document.querySelector('.time').innerHTML = bus.data.ibus[0]["text-ca"]
+                } else {
+                    document.querySelector('.time').innerHTML = "No information"
+                }
+            }, 3000);
+        })
+}
+
+setInterval(() => {
+    get()
+}, 30000);
